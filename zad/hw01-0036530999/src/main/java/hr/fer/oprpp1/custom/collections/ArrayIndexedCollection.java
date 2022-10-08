711b3daac311f82f66ba1ae9a786eb2411e1396e
Luka Sever <offblacc@pop-os.localdomain>
2022-10-08 16:25:15 +0200

    late committing problem2

--- zad/hw01-0036530999/src/main/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollection.java
+++ zad/hw01-0036530999/src/main/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollection.java
@@ -1,11 +1,13 @@
 package hr.fer.oprpp1.custom.collections;
 
 // not implementing addAll here because it has a defined behaviour in Collection class
-// and here we were to implement methods with undefined behaviour
+// and here we were to implement methods with undefined behaviour # TODO implement addAll ?
 
+// TODO check do constructors set size correctly
+// TODO check that you use .equals(), not ==, where necessary
 public class ArrayIndexedCollection extends Collection {
     private int size;
-    private Object[] elements; // TODO gonna need to check if actual len > size var
+    private Object[] elements;
 
     public ArrayIndexedCollection() {
         this(16);
@@ -16,8 +18,8 @@ public class ArrayIndexedCollection extends Collection {
             throw new IllegalArgumentException("Array size should be at least 1!");
         }
 
-        size = initialCapacity;
-        elements = new Object[size];
+        size = 0;
+        elements = new Object[initialCapacity];
     }
 
     public ArrayIndexedCollection(Collection other) {
@@ -33,8 +35,8 @@ public class ArrayIndexedCollection extends Collection {
             initialCapacity = other.size();
         }
 
-        size = initialCapacity;
-        elements = new Object[size];
+        size = other.size();
+        elements = new Object[initialCapacity];
         addAll(other);
     }
     // s novim definiranim ponašanjem get, insert
@@ -50,29 +52,25 @@ public class ArrayIndexedCollection extends Collection {
             throw new NullPointerException();
         }
 
-        boolean fullArray = true;
-        for (int i = 0; i < size(); i++) {
-            if (elements[i] == null) {
-                fullArray = false;
-                elements[i] = value;
-                return;
-            }
-        }
-
-        if (fullArray) {
-            Object[] newElements = new Object[2 * size()];
-            int i = 0;
+        int i = 0;
+        if (size == elements.length) {
+            Object[] newElements = new Object[2 * elements.length];
             for (Object v : elements) {
                 newElements[i++] = v;
             }
             elements = newElements;
+        } else {
+            while (elements[i++] != null)
+                ;
+            i--; // because postfix++ set i to one too many
         }
+        elements[i] = value;
     }
 
     @Override
     public boolean contains(Object value) { // it is OK to ask if collection contains null
-        for (Object o : elements) {
-            if (o.equals(value)) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(value)) {
                 return true;
             }
         }
@@ -84,7 +82,7 @@ public class ArrayIndexedCollection extends Collection {
             return -1;
         }
 
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < elements.length; i++) {
             if (elements[i].equals(value)) {
                 return i;
             }
@@ -93,17 +91,7 @@ public class ArrayIndexedCollection extends Collection {
         return -1;
     }
 
-    @Override
-    public boolean remove(Object value) {
-        int index = indexOf(value);
-        if (index == -1) {
-            return false;
-        }
-        elements[index] = null;
-        return true;
-    }
-
-    @Override // TODO should skip null values, right ?
+    @Override // TODO fix this, size lets you know the numNotNull xd stupidass
     Object[] toArray() {
         int numNotNull = 0;
         for (Object o : elements) {
@@ -124,44 +112,53 @@ public class ArrayIndexedCollection extends Collection {
         return arr;
     }
 
-    public void forEach(Processor processor) {
-        for (Object o : elements) {
-            processor.process(o);
-        }
-    }
-
     @Override
     public void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < elements.length; i++) {
             elements[i] = null;
         }
     }
 
     public Object get(int index) {
-        if (index < 0 || index > size() - 1) {
+        if (index < 0 || index > elements.length - 1) {
             throw new IndexOutOfBoundsException();
         }
 
         return elements[index];
     }
 
+    @Override
+    public boolean remove(Object value) {
+        int index = indexOf(value);
+        if (index == -1) {
+            return false;
+        }
+        shiftLeft(index);
+        return true;
+    }
+
     public void remove(int index) {
-        if (index < 0 || index > size() - 1) {
+        if (index < 0 || index > size - 1) {
             throw new IndexOutOfBoundsException();
         }
+        shiftLeft(index);
+    }
+
+    private void shiftLeft(int index) {
         index++;
-        for (; index < size(); index++) {
+        for (; index < size; index++) {
             elements[index - 1] = elements[index];
         }
+        elements[size - 1] = null;
     }
 
     public void insert(Object value, int position) {
-        if (position < 0 || position > size()) {
+        if (position < 0 || position > size) {
             throw new IndexOutOfBoundsException();
         }
 
-        if (!(elements[size() - 1] == null)) {
-            Object[] newElements = new Object[2 * size()];
+        if (elements[elements.length - 1] != null) {
+            Object[] newElements = new Object[2 * elements.length];
             int i = 0;
             for (Object v : elements) {
                 newElements[i++] = v;
@@ -169,11 +166,30 @@ public class ArrayIndexedCollection extends Collection {
             elements = newElements;
         }
 
-        for (int i = size() - 1; i > position; i--) {
+        for (int i = size - 1; i > position; i--) {
             elements[i] = elements[i - 1];
         }
+        
+        elements[position] = null;
+        add(value); // add finds first empty space - will find the newly created null
+    }
 
-        add(value);
+    @Override
+    public void forEach(Processor processor) {
+        for (Object o : elements) {
+            processor.process(o);
+        }
+    }
+
+    @Override
+    public void addAll(Collection other) {
+        class LocalProcessor extends Processor {
+            public void process(Object value) {
+                add(value);
+                size++;
+            }
+        }
+        ((ArrayIndexedCollection) other).forEach(new LocalProcessor());
     }
 
 }
